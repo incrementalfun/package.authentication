55c8b660cff41330ddb897f0c5bb09136e3028c3
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-11-04 16:43:15 +0100

    Updated summaries

--- src/Incremental.Common.Configuration.Authentication/IncrementalScopes.cs
+++ src/Incremental.Common.Configuration.Authentication/IncrementalScopes.cs
@@ -5,7 +5,13 @@
     /// </summary>
     public struct IncrementalScopes
     {
+        /// <summary>
+        /// Core scope.
+        /// </summary>
         public static string Core => nameof(Core);
+        /// <summary>
+        /// Extension scope.
+        /// </summary>
         public static string Extension => nameof(Extension);
     }
 }
