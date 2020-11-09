93105cf9da11ca1cb0a8db69da390e1a37218d5e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-11-09 14:24:10 +0100

    Added extension methods to claims

---
+++ src/Incremental.Common.Configuration.Authentication/ClaimsPrincipalExtension.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace Incremental.Common.Configuration.Authentication
+{
+    /// <summary>
+    /// Extension methods to simplify claims usage.
+    /// </summary>
+    public static class ClaimsPrincipalExtension
+    {
+        /// <summary>
+        /// Get user id.
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <returns></returns>
+        public static string GetId(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+        
+        /// <summary>
+        /// Get user email.
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <returns></returns>
+        public static string GetEmail(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimTypes.Email);
+        }
+
+        /// <summary>
+        /// get user username.
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <returns></returns>
+        public static string GetUsername(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(JwtRegisteredClaimNames.Sub);
+        }
+    }
+}
