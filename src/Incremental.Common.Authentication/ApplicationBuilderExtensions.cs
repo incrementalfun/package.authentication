82d4022219dfcc84ff87216ae006091ba76edaea
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-13 17:14:09 +0100

    Added UseCommonCors extension

---
+++ src/Incremental.Common.Authentication/ApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Incremental.Common.Authentication
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseCommonCors(IApplicationBuilder app, IConfiguration configuration)
+        {
+            app.UseCors(options =>
+            {
+                options.WithOrigins(configuration["SPA_BASE_URI"])
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .WithExposedHeaders("x-pagination");
+            });
+
+            return app;
+        }
+    }
+}
