093dee75246da704cb4af2cec7eadfbcded3ef5a
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-01-14 17:08:20 +0200

    feat: update signature of GenerateToken in ITokenService

--- src/Incremental.Common.Authentication.Jwt/ITokenService.cs
+++ src/Incremental.Common.Authentication.Jwt/ITokenService.cs
@@ -4,7 +4,7 @@ namespace Incremental.Common.Authentication.Jwt;
 
 public interface ITokenService
 {
-    Task<JwtToken> GenerateToken(string userId, string? audience, Claim[]? additionalClaims = default);
+    Task<JwtToken> GenerateToken(string userId, string? audience = default, Claim[]? additionalClaims = default);
     
     Task<JwtToken?> RefreshToken(JwtToken token, Claim[]? additionalClaims = default);
 }
