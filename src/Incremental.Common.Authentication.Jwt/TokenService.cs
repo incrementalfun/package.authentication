cdb802bb07b48c5132dd547f2543594e507d2ada
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-01-18 19:45:47 +0200

    feat: implement RevokeRefreshTokens in TokenService

--- src/Incremental.Common.Authentication.Jwt/TokenService.cs
+++ src/Incremental.Common.Authentication.Jwt/TokenService.cs
@@ -2,28 +2,34 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Incremental.Common.Authentication.Jwt;
 
-public class TokenService<TUser> : ITokenService where TUser : IdentityUser
+public class TokenService<TUser, TContext> : ITokenService
+    where TUser : IdentityUser
+    where TContext : IdentityDbContext<TUser>
 {
-    private readonly ILogger<TokenService<TUser>> _logger;
+    private readonly ILogger<TokenService<TUser, TContext>> _logger;
     private readonly UserManager<TUser> _userManager;
+    private readonly TContext _context;
     private readonly JwtBearerOptions _jwtBearerOptions;
+    private readonly TokenServiceOptions _tokenServiceOptions;
     private readonly JwtSecurityTokenHandler _securityTokenHandler;
     private readonly SigningCredentials _signingCredentials;
     private readonly string _issuer;
 
-    private const string APPLICATION_LOGIN_PROVIDER = "application_identity";
-    
-    public TokenService(ILogger<TokenService<TUser>> logger, UserManager<TUser> userManager, IOptions<JwtBearerOptions> jwtBearerOptions)
+    public TokenService(ILogger<TokenService<TUser, TContext>> logger, UserManager<TUser> userManager, TContext context, IOptions<JwtBearerOptions> jwtBearerOptions, IOptions<TokenServiceOptions> tokenServiceOptions)
     {
         _logger = logger;
         _userManager = userManager;
+        _context = context;
         _jwtBearerOptions = jwtBearerOptions.Value;
+        _tokenServiceOptions = tokenServiceOptions.Value;
 
         _securityTokenHandler = new JwtSecurityTokenHandler();
         _signingCredentials = new SigningCredentials(_jwtBearerOptions.TokenValidationParameters.IssuerSigningKey, SecurityAlgorithms.HmacSha256);
@@ -44,11 +50,8 @@ public class TokenService<TUser> : ITokenService where TUser : IdentityUser
 
         var token = GenerateJwtSecurityToken();
         var refreshToken = Guid.NewGuid();
-        await _userManager.SetAuthenticationTokenAsync(
-            user: user,
-            loginProvider: "application_identity",
-            tokenName: APPLICATION_LOGIN_PROVIDER, 
-            tokenValue: DateTime.UtcNow.AddDays(30).Ticks.ToString());
+        
+        await StoreRefreshTokenAsync(refreshToken);
 
         return new JwtToken
         {
@@ -58,7 +61,22 @@ public class TokenService<TUser> : ITokenService where TUser : IdentityUser
         
         JwtSecurityToken GenerateJwtSecurityToken()
         {
-            return new JwtSecurityToken(_issuer, audience, claims, DateTime.UtcNow, DateTime.UtcNow.AddDays(1), _signingCredentials);
+            return new JwtSecurityToken(
+                issuer: _issuer, 
+                audience: audience, 
+                claims: claims,
+                notBefore: DateTime.UtcNow, 
+                expires: DateTime.UtcNow.AddMinutes(_tokenServiceOptions.TokenLifetime), 
+                signingCredentials: _signingCredentials);
+        }
+
+        async Task StoreRefreshTokenAsync(Guid guid)
+        {
+            await _userManager.SetAuthenticationTokenAsync(
+                user: user,
+                loginProvider: _tokenServiceOptions.ApplicationLoginProvider,
+                tokenName: EncodeRefreshToken(guid),
+                tokenValue: DateTime.UtcNow.AddMinutes(_tokenServiceOptions.RefreshTokenLifetime).Ticks.ToString());
         }
     }
     
@@ -105,13 +123,13 @@ public class TokenService<TUser> : ITokenService where TUser : IdentityUser
 
         async Task<DateTime?> RetrieveRefreshTokenLifetimeAsync()
         {
-            var information = await _userManager.GetAuthenticationTokenAsync(user, APPLICATION_LOGIN_PROVIDER, RefreshToken(token.RefreshToken));
+            var information = await _userManager.GetAuthenticationTokenAsync(user, _tokenServiceOptions.ApplicationLoginProvider, EncodeRefreshToken(token.RefreshToken));
             return information is null ? default(DateTime?) : new DateTime(Convert.ToInt64(information));
         }
 
         async Task RemoveRefreshTokenAsync()
         {
-            await _userManager.RemoveAuthenticationTokenAsync(user, APPLICATION_LOGIN_PROVIDER, RefreshToken(token.RefreshToken));
+            await _userManager.RemoveAuthenticationTokenAsync(user, _tokenServiceOptions.ApplicationLoginProvider, EncodeRefreshToken(token.RefreshToken));
         }
 
         async Task<Claim[]?> RetrieveAdditionalClaimsAsync()
@@ -123,6 +141,19 @@ public class TokenService<TUser> : ITokenService where TUser : IdentityUser
             return jwtSecurityToken.Claims.Where(claim => !defaultClaimTypes.Contains(claim.Type)) as Claim[];
         }
     }
-    
-    private static string RefreshToken(Guid refreshTokenId) => $"refresh_token:{refreshTokenId}";
+
+    public async Task RevokeRefreshTokens(string userId)
+    {
+        var refreshTokens = await _context.UserTokens
+            .Where(token => token.UserId == userId)
+            .Where(token => token.LoginProvider == _tokenServiceOptions.ApplicationLoginProvider)
+            .Where(token => token.Name.StartsWith("refresh_token"))
+            .ToListAsync();
+
+        _context.RemoveRange(refreshTokens);
+
+        await _context.SaveChangesAsync();
+    }
+
+    private static string EncodeRefreshToken(Guid refreshTokenId) => $"refresh_token:{refreshTokenId}";
 }
