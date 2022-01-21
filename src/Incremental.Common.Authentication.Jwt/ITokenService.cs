4bf54ff60063e40b2b98d61e717ad8627ee32fd9
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-01-21 13:58:25 +0200

    feat: add overload to GenerateToken

--- src/Incremental.Common.Authentication.Jwt/ITokenService.cs
+++ src/Incremental.Common.Authentication.Jwt/ITokenService.cs
@@ -4,8 +4,9 @@ namespace Incremental.Common.Authentication.Jwt;
 
 public interface ITokenService
 {
-    Task<JwtToken> GenerateToken(string userId, IEnumerable<string>? audiences = default);
+    Task<JwtToken> GenerateToken(string? userId, IEnumerable<string>? audiences = default);
     
+    Task<JwtToken> GenerateToken(string userId, string audience);
     Task<JwtToken?> RefreshToken(JwtToken token);
 
     Task RevokeRefreshTokens(string userId);
