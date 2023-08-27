45f0d79bdfb3821c8ccb2dfdd23b4df1c441a5f2
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-08-27 15:50:34 +0300

    feat: make token lifetime configurable using JWT_LIFETIME

--- src/Incremental.Common.Authentication.Jwt/TokenServiceOptionsPostConfigureOptions.cs
+++ src/Incremental.Common.Authentication.Jwt/TokenServiceOptionsPostConfigureOptions.cs
@@ -12,8 +12,17 @@ public class TokenServiceOptionsPostConfigureOptions: IPostConfigureOptions<Toke
         _configuration = configuration;
     }
 
-    public void PostConfigure(string name, TokenServiceOptions options)
+    public void PostConfigure(string? name, TokenServiceOptions options)
     {
+        if (!string.IsNullOrWhiteSpace(_configuration["JWT_LIFETIME"]))
+        {
+            var lifetime = TimeSpan.FromMinutes(_configuration.GetValue<int>("JWT_LIFETIME"));
+            if (lifetime.TotalMinutes >= 1)
+            {
+                options.TokenLifetime = lifetime;
+            }
+        }
+        
         options.TokenIssuer ??= _configuration["JWT_TOKEN_ISSUER"];
         options.TokenSecurityKey ??= _configuration["JWT_TOKEN_SECURITY_KEY"];
     }
